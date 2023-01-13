5f66a2362d28353e4ab76daaa5fef82cc4b519d0
Breno Almeida Nunes de Souza <kbreno08@gmail.com>
2023-01-12 23:15:29 -0300

    Refatorando o componente de Formulario

--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -17,9 +17,7 @@
         <div
           class="is-flex is-align-items-center is-justify-content-space-between"
         >
-          <section>
-            <strong>{{ tempoDecorrido }}</strong>
-          </section>
+          <Cronometro :tempo="tempo" />
           <button class="button" @click="iniciar">
             <span class="icon">
               <i class="fas fa-play"></i>
@@ -40,18 +38,17 @@
 
 <script lang="ts">
 import { defineComponent } from "vue";
+import Cronometro from "./Cronometro.vue";
 
 export default defineComponent({
   name: "Formulario",
+  components: {
+    Cronometro
+  },
   data() {
     return {
       tempo: 0,
       cronometro: 0
-    }
-  },
-  computed:{
-    tempoDecorrido() : string {
-      return new Date(this.tempo * 1000).toISOString().substr(11,8);
     }
   },
   methods: {
