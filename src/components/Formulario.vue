80dd933dca691d1cc1a26240e217067573fe98d9
Breno Almeida Nunes de Souza <kbreno08@gmail.com>
2023-01-16 23:49:08 -0300

    Refatorando para redistribuir responsabilidades dos componentes

--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -14,23 +14,7 @@
         />
       </div>
       <div class="column">
-        <div
-          class="is-flex is-align-items-center is-justify-content-space-between"
-        >
-          <Cronometro :tempo="tempo" />
-          <button class="button" @click="iniciar">
-            <span class="icon">
-              <i class="fas fa-play"></i>
-            </span>
-            <span>play</span>
-          </button>
-          <button class="button" @click="finalizar">
-            <span class="icon">
-              <i class="fas fa-stop"></i>
-            </span>
-            <span>stop</span>
-          </button>
-        </div>
+        <Temporizador />
       </div>
     </div>
   </div>
@@ -38,28 +22,13 @@
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import Cronometro from "./Cronometro.vue";
+import Temporizador from "./Temporizador.vue";
 
 export default defineComponent({
   name: "Formulario",
   components: {
-    Cronometro
+    Temporizador
   },
-  data() {
-    return {
-      tempo: 0,
-      cronometro: 0
-    }
-  },
-  methods: {
-    iniciar(){
-      this.cronometro = setInterval(() => {
-        this.tempo +=1;
-      }, 1000)
-    },
-    finalizar(){
-      clearInterval(this.cronometro);
-    }
-  }
+  
 });
 </script>
